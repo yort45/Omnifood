14f4f161b6de01a90ab7dbefc68affd138c5cb10
Troy Turner <57537301+yort45@users.noreply.github.com>
2019-12-02 15:14:22 -0600

    widescreen @media
    added a section to adjust site for widescreens like at the office for 1920x1080. Ive added nothing there yet...

---
+++ resources/js/script.js
@@ -0,0 +1,19 @@
+//must have this to start, all goes inside this
+$(document).ready(function(){
+
+$('h1').click(function() {
+	$(this).css('background-color', '#ff0000')
+})
+
+
+
+
+
+
+
+
+
+
+
+});
+//must have that to finish, all goes inside this
