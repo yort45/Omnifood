2be3ae269452d368ad43dd1624aae82328e3ea03
Troy Turner <57537301+yort45@users.noreply.github.com>
2019-12-03 16:53:31 -0600

    update to new vid

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,7 +1,21 @@
 //must have this to start, all goes inside this
 $(document).ready(function(){
 
+	$('.js--section-features').waypoint(function(direction) { 
+		if (direction == "down") {
+			$('nav').addClass('sticky');
+		} else {
+			$('nav').removeClass('sticky');
+		}
+	})
 
+	/*
+	var waypoints = $('#handler-first').waypoint(function(direction) {
+	  notify(this.element.id + ' hit 25% from top of window') 
+	}, {
+	  offset: '25%'
+	})
+	*/
 
 
 
